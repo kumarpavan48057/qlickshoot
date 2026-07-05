0571b5664df13d83e012d9fbb0c8ebd359b55b8b
kumarpavan48057 <kumarpavan48057@gmail.com>
2026-07-05 22:11:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Qlickshootpage/', // Replace if your repository name changes
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
