457520925b315e975a9430da4846bb8cec56de93
kumarpavan48057 <kumarpavan48057@gmail.com>
2026-07-05 22:22:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Qlickshootpage/', // Replace if your repository name changes
+  base: '/qlickshoot/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
